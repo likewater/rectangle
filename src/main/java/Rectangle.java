9b505e637430c9dc4d58ac90ba37ca2ce33a20b9
Blake Womack and Grace Stuart <grace@>
2017-03-22 08:51:59 -0700

    add class content.

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Rectangle {
+  private int mLength;
+  private int mWidth;
+  private static List<Rectangle> instances = new ArrayList<Rectangle>();
+  }
+
+  public int getLength() {
+    return mLength;
+  }
+
+  public int getWidth() {
+    return mWidth;
+  }
+
+  public boolean isSquare() {
+    return mLength == mWidth;
+  }
+
+  public int area() {
+    return mLength * mWidth;
+  }
+
+}
